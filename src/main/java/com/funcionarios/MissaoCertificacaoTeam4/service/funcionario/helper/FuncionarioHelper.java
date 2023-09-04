7b98e3503613e5ff696f7c84804fdc3aa66fe457
Fernando <fernandorff92@gmail.com>
2023-09-03 21:10:36 -0300

    .

--- src/main/java/com/funcionarios/MissaoCertificacaoTeam4/service/funcionario/helper/FuncionarioHelper.java
+++ src/main/java/com/funcionarios/MissaoCertificacaoTeam4/service/funcionario/helper/FuncionarioHelper.java
@@ -2,7 +2,6 @@ package com.funcionarios.MissaoCertificacaoTeam4.service.funcionario.helper;
 
 import com.funcionarios.MissaoCertificacaoTeam4.entity.Funcionario;
 import com.funcionarios.MissaoCertificacaoTeam4.repository.FuncionarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,8 +9,11 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class FuncionarioHelper {
 
-    @Autowired
-    private FuncionarioRepository funcionarioRepository;
+    private final FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioHelper(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
 
     public Funcionario retornarFuncionarioValidoPorId(Long id) {
 
