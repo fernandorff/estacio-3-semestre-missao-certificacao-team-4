c53fa49b46404f71987c186beb4a137fc088e909
gilsonmneto <gilsonmneto@yahoo.com.br>
2023-09-14 17:12:52 -0300

    Responses e Requests

---
+++ src/main/java/com/funcionarios/MissaoCertificacaoTeam4/dto/response/ProcessoResponse.java
@@ -0,0 +1,16 @@
+package com.funcionarios.MissaoCertificacaoTeam4.dto.response;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ProcessoResponse {
+    private Integer id;
+    private String numero;
+
+}
